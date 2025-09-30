bbbcbd23a566b0f36df789570b34f3a7e8393fb4
Noa-Ble <Noab@jether-energy.com>
2025-09-30 15:16:10 +0300

    Add support for gcpubsub broker (#2)
    Implement new broker for GCP Pub/Sub

--- setup.py
+++ setup.py
@@ -56,7 +56,13 @@ setup(
     classifiers=classifiers,
     packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=get_requirements('default.txt'),
-    extras_require={':python_version == "2.7"': ['futures']},
+    extras_require={
+        ':python_version == "2.7"': ['futures'],
+        'gcppubsub': [
+            'google-cloud-pubsub>=2.0.0',
+            'google-cloud-monitoring>=2.0.0'
+        ]
+    },
     test_suite="tests",
     tests_require=get_requirements('test.txt'),
     package_data={'flower': ['templates/*', 'static/*.*',
